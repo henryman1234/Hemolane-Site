505113bcaa257bf82806de4bbb51b7fc3db7df2d
Henry Euloge <henrynomo68@gmail.com>
2025-12-15 22:38:37 +0100

    premier commit du projet

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { div } from "framer-motion/client";
+import React from "react";
+import "./sass/App.scss";
+
+
+function App () {
+  return (
+    <div  className="page">
+
+      <div className="page-wrapper">
+        <div>Bonsoir les gens</div>
+        <div>Bonjour les gens</div>
+      </div>
+    
+    </div>
+  )
+}
+
+export  default App
