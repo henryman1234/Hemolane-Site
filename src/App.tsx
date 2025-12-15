404a8deb5caf9ab0fe9dc5b9a800af79cd1f4cf9
Henry Euloge <henrynomo68@gmail.com>
2025-12-15 22:45:30 +0100

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import { div } from "framer-motion/client";
-import React from "react";
+import React, { useState } from "react";
 import "./sass/App.scss";
+import Navbar from "./components/navbar/Navbar";
 
 
 function App () {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false)
+  const [activeSecton, setIsActiveSection] = useState(false)
+
   return (
     <div  className="page">
-
       <div className="page-wrapper">
+        <Navbar/>
+        <main>
         <div>Bonsoir les gens</div>
         <div>Bonjour les gens</div>
+        </main>
+
       </div>
     
     </div>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,3 +2,14 @@ import React from "react";
 import "./navbar.scss";
 
 
+const Navbar = function () {
+    return (
+        <nav>
+            <div className="navcontainer">
+                Bonjour les gens, ici c'est la navbar
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
